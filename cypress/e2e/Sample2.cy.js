bc266881e6eb74d99cf76f2ade1668288cf55577
Francis143 <francislaavi@gmail.com>
2024-08-20 22:39:49 +0530

    Second

--- cypress/e2e/Sample2.cy.js
+++ cypress/e2e/Sample2.cy.js
@@ -1,30 +1,28 @@
-// // cypress/e2e/your-test.spec.js
+// cypress/e2e/your-test.spec.js
 
-// // const filePath = 'cypress/fixtures/excelData.xlsx';
-// // const sheetName = 'Sheet1'; // Name of the sheet to update
-// import Index from "./PageObjects/Index";
-// import LogingPage from "./PageObjects/LoginPage";
-// for (let i = 2; i <= 7; i++) {
+// const filePath = 'cypress/fixtures/excelData.xlsx';
+// const sheetName = 'Sheet1'; // Name of the sheet to update
+import Index from "./PageObjects/Index";
+import LogingPage from "./PageObjects/LoginPage";
+for (let i = 2; i <= 7; i++) {
 
 
-//   describe(`Write Specific Cell to Excel Test-${i}`, () => {
-//     it('should write a value to a specific cell in an Excel file',{retries:{
-//       openMode:2
-//     }}, () => {
-//       const lp = new LogingPage()
-//       cy.visit("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login")
-//       lp.userName('Admin')
-//       lp.password("admin123")
-//       lp.loginBtn()
+  describe(`Write Specific Cell to Excel Test-${i}`, () => {
+    it('should write a value to a specific cell in an Excel file', () => {
+      const lp = new LogingPage()
+      cy.visit("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login")
+      lp.userName('Admin')
+      lp.password("admin123")
+      lp.loginBtn()
 
-//       //Verify the Home page url contains "Index"
-//       cy.url().should('include', '/index')
-//       const ip = new Index()
-//       ip.adminMenu()
-//       ip.PIMMenu()
-//       ip.LeaveMenu()
-//       ip.TimeMenu()
-//       // ip.Recruitment()
+      //Verify the Home page url contains "Index"
+      cy.url().should('include', '/index')
+      const ip = new Index()
+      ip.adminMenu()
+      ip.PIMMenu()
+      ip.LeaveMenu()
+      ip.TimeMenu()
+      // ip.Recruitment()
 
 
 
@@ -49,15 +47,15 @@
 
 
 
-//       const filePath = 'cypress/fixtures/excelData.xlsx';
-//       const sheetName = 'Sheet1'; // Name of the sheet to update
-//       const cell = `S${i}`; // Cell address in format "A1"
-//       const value = 'Engineer';
+      const filePath = 'cypress/fixtures/excelData.xlsx';
+      const sheetName = 'Sheet1'; // Name of the sheet to update
+      const cell = `S${i}`; // Cell address in format "A1"
+      const value = 'Francis Sanjon';
 
-//       cy.task('writeToExcel', { filePath, sheetName, cell, value }).then(() => {
-//         cy.log(`Value "${value}" written to cell ${cell} in sheet "${sheetName}".`);
-//       });
-//     });
-//   });
-// }
+      cy.task('writeToExcel', { filePath, sheetName, cell, value }).then(() => {
+        cy.log(`Value "${value}" written to cell ${cell} in sheet "${sheetName}".`);
+      });
+    });
+  });
+}
 
