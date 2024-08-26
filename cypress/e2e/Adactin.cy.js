6e0ee0ec14934e9a196024b69fc75feed1b525de
Francis143 <francislaavi@gmail.com>
2024-08-26 22:02:22 +0530

    Update code in Adactin.cy.js

--- cypress/e2e/Adactin.cy.js
+++ cypress/e2e/Adactin.cy.js
@@ -5,30 +5,41 @@
 import Index from "./PageObjects/Index";
 import LogingPage from "./PageObjects/LoginPage";
 import Searchhotel from "./PageObjects/SearchHotel";
-for (let i = 2; i <= 20; i++) {
+for (let i = 2; i <= 2; i++) {
 
 
   describe(`Write Specific Cell to Excel Test-${i}`, () => {
     before('Before', () => {
       cy.log('this is before block')
-      cy.task('readXlsx', { file: 'cypress/fixtures/excelData.xlsx', sheet: "Sheet1" }).then((rows) => {
+      cy.task('readXlsx', { file: 'cypress/fixtures/Adactin.xlsx', sheet: "Sheet1" }).then((rows) => {
         let rowsLength = rows.length;
-        cy.writeFile("cypress/fixtures/xlsxData.json", { rows })
+        cy.writeFile("cypress/fixtures/Adactin.json", { rows })
       })
     })
     it('should write a value to a specific cell in an Excel file', () => {
       const lp = new LogingPage()
       const sh = new Searchhotel()
       cy.visit("https://adactinhotelapp.com/index.php")
-      lp.userName("Francis3535")
-      lp.password("Admin@123")
+      cy.fixture('Adactin').then((data) => {
+
+        if(data.rows[i-2].username!=''){
+          lp.userName(data.rows[i-2].username)
+          lp.password("Francis@123")
+
+        }
+        
+
+      })
+
+      // lp.userName("Francis3535")
+      // lp.password("Francis@123")
       lp.loginBtn()
       sh.location("Sydney")
       sh.hotel("Hotel Creek")
       sh.roomType("Double")
 
-      cy.get(':nth-child(3) > [align="right"]').then((data)=>{
-        const x=data.text();
+      cy.get(':nth-child(3) > [align="right"]').then((data) => {
+        const x = data.text();
         cy.log(x)
       })
 
@@ -56,10 +67,10 @@ for (let i = 2; i <= 20; i++) {
 
 
 
-      const filePath = 'cypress/fixtures/excelData.xlsx';
+      const filePath = 'cypress/fixtures/Adactin.xlsx';
       const sheetName = 'Sheet1'; // Name of the sheet to update
-      const cell = `S${i}`; // Cell address in format "A1"
-      const value = 'Francis Sanjon';
+      const cell = `C${i}`; // Cell address in format "A1"
+      const value = 'Success';
 
       cy.task('writeToExcel', { filePath, sheetName, cell, value }).then(() => {
         cy.log(`Value "${value}" written to cell ${cell} in sheet "${sheetName}".`);
--- cypress/e2e/PageObjects/LoginPage.js
+++ cypress/e2e/PageObjects/LoginPage.js
@@ -1,14 +1,14 @@
 class LogingPage{
 
 userName(value){
-    cy.get(':nth-child(2) > .oxd-input-group > :nth-child(2) > .oxd-input').type(value)
+    cy.get('#username').type(value)
 }
 password(value){
-    cy.get(':nth-child(3) > .oxd-input-group > :nth-child(2) > .oxd-input').type(value)
+    cy.get('#password').type(value)
 }
 
 loginBtn(){
-    cy.get('.oxd-button').click()
+    cy.get('#login').click()
 }
 
 
